ec28c493364f791832b922df886862c8d6f60c21
yraderyn <40969757+yraderyn@users.noreply.github.com>
2018-07-09 11:07:38 +0200

    Update zadatak3.py

--- zadatak3.py
+++ zadatak3.py
@@ -1,12 +1,12 @@
 devojka_iz_petnice = "Da je ljubav nauka\ndobio bih Nobela\nA da je ljubav knjiga\nuzeo bih NIN-a\n\nDevojke iz Petnice\nredom su pametnice\nA jedna među njima\nsad moje srce ima\n\nŽelim da sam projekat\ni da sam joj u mislima\nmrtvi Vizantinac\niz srednjeg veka pisac\nželim da sam bitan\nu njenim poslovima\n\nDevojka iz Petnice\nona meni znači sve\ni kada me se seti\nja hoću da poletim\n\nDevojka iz Petnice\nu koju sam zaljubljen\nsad uči tamo negde\nDa li pamti mene?\n\nHoću da sam tema\nU nečemu što sprema\nMrtvi Vizantinac\niz srednjeg veka pisac\nželim da sam bitan\nu njenim poslovima"
 lista_stihova = devojka_iz_petnice.split('\n')
-konacna = []
+konacna = list()
 for stih in lista_stihova:
 	reciustihu = stih.split(' ')
 	for rec in reciustihu:
 		konacna.append(rec)
-	reciustihu = [ ]
-k2 = []
+	reciustihu = list()
+k2 = list()
 for a in konacna:
 	if a == "Devojke":
 		a = "Dečaci"
