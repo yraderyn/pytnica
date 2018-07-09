26ec0a20461e07e65337f7d4ef8894b2d9997b21
yraderyn <40969757+yraderyn@users.noreply.github.com>
2018-07-09 10:55:25 +0200

    Update zadatak1.py

--- zadatak1.py
+++ zadatak1.py
@@ -1,15 +1,20 @@
 devojka_iz_petnice = "Da je ljubav nauka\ndobio bih Nobela\nA da je ljubav knjiga\nuzeo bih NIN-a\n\nDevojke iz Petnice\nredom su pametnice\nA jedna među njima\nsad moje srce ima\n\nŽelim da sam projekat\ni da sam joj u mislima\nmrtvi Vizantinac\niz srednjeg veka pisac\nželim da sam bitan\nu njenim poslovima\n\nDevojka iz Petnice\nona meni znači sve\ni kada me se seti\nja hoću da poletim\n\nDevojka iz Petnice\nu koju sam zaljubljen\nsad uči tamo negde\nDa li pamti mene?\n\nHoću da sam tema\nU nečemu što sprema\nMrtvi Vizantinac\niz srednjeg veka pisac\nželim da sam bitan\nu njenim poslovima"
 aa = devojka_iz_petnice.lower()
 lista_stihova = aa.split('\n')
-konacna = []
+konacna = list()
 for stih in lista_stihova:
 	reciustihu = stih.split(' ')
 	for rec in reciustihu:
-		konacna.append(rec)
-	reciustihu = [ ]
+		if rec != '':
+			rec = rec.strip("?")
+			konacna.append(rec)
+	reciustihu = list()
 print(len(konacna))
-neponovljene = [ ]
+# prva varijanta:
+neponovljene = list()
 for i in konacna:
 	if i not in neponovljene:
 		neponovljene.append(i)
+# druga varijanta:
+# neponovljene = set(y)
 print(len(neponovljene))
