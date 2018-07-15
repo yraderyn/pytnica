2516969e5aac45fed15791f9dfffd9ed1610a8eb
yraderyn <40969757+yraderyn@users.noreply.github.com>
2018-07-15 12:21:36 +0200

    Update 3.py

--- proki/3.py
+++ proki/3.py
@@ -1,6 +1,6 @@
 import re
 niskapogrešnih = str()
-listaprefiksa = ['protivu', 'protiv', 'nadri', 'preda', 'nada', 'poda', 'beza', 'raza', 'polu', 'pred', 'pret', 'vele', 'samo', 'mimo', 'među', 'bez', 'bes', 'bež', 'beš', 'van', 'pod', 'pot', 'pra', 'pre', 'pri', 'raz', 'ras', 'raž', 'raš', 'nad', 'nat', 'naj', 'nuz', 'nus', 'oda', 'iza', 'oba', 'pa', 'po', 'do', 'iz', 'is', 'iž', 'iš', 'uz', 'us', 'už', 'uš', 'na', 'ne', 'su', 'sa', 'od', 'ot', 'ob', 'o', 'u', 's', 'z']
+listaprefiksa = ['protivu', 'protiv', 'nadri', 'preda', 'nada', 'poda', 'beza', 'raza', 'polu', 'pred', 'pret', 'vele', 'samo', 'mimo', 'među', 'bez', 'bes', 'bež', 'beš', 'van', 'pod', 'pot', 'pra', 'pre', 'pri', 'raz', 'ras', 'raž', 'raš', 'nad', 'nat', 'naj', 'nuz', 'nus', 'oda', 'iza', 'oba', 'pa', 'po', 'do', 'iz', 'is', 'iž', 'iš', 'uz', 'us', 'už', 'uš', 'na', 'ne', 'su', 'sa', 'od', 'za', 'ot', 'ob', 'o', 'u', 's', 'z']
 with open("2.txt", "r", encoding="utf-8") as Ulaz:
 	lista = [red[:-1] for red in Ulaz.readlines()]
 	niska = ' '.join(lista)
@@ -24,4 +24,3 @@ with open("2.txt", "r", encoding="utf-8") as Ulaz:
 							else:
 								pass
 			Pogrešan.write(niskapogrešnih)
-			
